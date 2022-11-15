d4c072ede205684e9f4e6bb36f59f060a35edbff
tranh <henri-bergson.tran@etu.u-paris.fr>
2022-11-15 16:40:27 +0100

    verification et test appel de la commande entree

--- slash.c
+++ slash.c
@@ -53,7 +53,19 @@ int main(int argc, char **argv) {
             i++;
             token = strtok(NULL, " ");
         }
-
+        //verification et appel de la commande
+        if(strcmp(tokens[0],"exit") == 0){
+            printf("lancement de la fonction exit\n");
+        }
+        else if(strcmp(tokens[0],"cd") == 0){
+            printf("lancement de la fonction cd\n");
+        }
+        else if(strcmp(tokens[0],"pwd") == 0){
+            printf("lancement de la fonction pwd\n");
+        }
+        else{
+            printf("Erreur , aucune commande reconnue\n");
+        }
         // readline fait un malloc à chaque fois donc on dois le free à la fin
         free(tmp);
         free(input);
